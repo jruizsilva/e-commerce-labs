a47f8cffa5b4653d2511e7e8848594e224efd38c
jruizsilva <ruizsilvajonathan@gmail.com>
2022-06-06 16:34:40 -0300

    access control

--- api/server.js
+++ api/server.js
@@ -20,6 +20,21 @@ class Server {
     // CORS
     this.app.use(cors());
 
+    // Access control
+    this.app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Credentials", "true");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, DELETE"
+      );
+      next();
+    });
+
     // Lectura y parseo del body
     this.app.use(express.json());
 
