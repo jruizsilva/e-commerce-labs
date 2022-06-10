e59b9471dbf06d4652eba83d1a7225ea7c2b658f
Tomas Holgado <creadordecaminos@gmail.com>
2022-06-10 13:51:03 -0300

    feature: get details from product. Added component that shows the details

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; //BrowserRouter va (y ya está) en index.js
 
 // importo los componentes que vamos a renderizar
 import Home from './components/Home/Home.jsx';
+import ProductDetails from "./components/ProductDetails/ProductDetails.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/details/:productId" element={<ProductDetails />} />
     </Routes> 
   );
 }
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const getAllProducts = () => {
   return function(dispatch){
     return(
-      axios.get(`http://localhost:3001/api/products`)
+      axios.get(`http://localhost:3001/api/products`) // edit Tomas: cambio el port de 3000 a 3001 para que traiga los productos
         .then((resp)=>{
           dispatch({type: GET_ALL_PRODUCTS, payload: resp.data})
         })
