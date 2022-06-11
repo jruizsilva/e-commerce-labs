9e2d6dddbcee58e561e0f9461fc7f28ca9a011a6
jruizsilva <ruizsilvajonathan@gmail.com>
2022-06-11 09:38:52 -0300

    homepage guest design finished

--- client/src/components/FilterReset/FilterReset.js
+++ client/src/components/FilterReset/FilterReset.js
@@ -5,13 +5,20 @@ import style from "./FilterReset.module.css";
 export default function FilterReset() {
   const [params, setParams] = useSearchParams();
 
-  const handleClick = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
     setParams({ reset: true });
   };
   return (
     <div className={style.container}>
       <h4 className={style.title}>Reset</h4>
-      <button type="reset" className={style.link} onClick={handleClick}>
+      <button className={style.button} onClick={handleClick}>
+        <span
+          className="material-symbols-rounded"
+          style={{ fontSize: "18px", marginRight: "6px" }}
+        >
+          restart_alt
+        </span>
         Reset
       </button>
     </div>
