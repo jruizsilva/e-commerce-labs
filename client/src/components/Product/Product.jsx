acda5c1e76f956f7109b1d16fa1c57eb367534f2
jruizsilva <ruizsilvajonathan@gmail.com>
2022-06-10 21:26:41 -0300

    refactor ProductCard

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -5,13 +5,17 @@ const Product = ({ data }) => {
   return (
     <li className={style.productItem}>
       <article className={style.productContainer}>
-        <img src={data.image} alt="" />
-        <h3>$ {data.price}</h3>
-        <label htmlFor="">condicion</label>
-        <div className={style.btnAddCart}>
-          <button>Add cart</button>
+        <div className={style.image_container}>
+          <img src={data.image} className={style.image} alt="product" />
         </div>
-      </article></li>
+        <div className={style.description}>
+          <h3 className={style.title}>$ {data.price}</h3>
+          <p className={style.p}>condicion</p>
+          <button className={style.btnAddCart}>Agregar al carrito</button>
+
+        </div>
+      </article>
+    </li>
   );
 };
 
