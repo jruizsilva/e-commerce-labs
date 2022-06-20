77278649b0fadc482255eee0bc2fd14805304fd6
jruizsilva <ruizsilvajonathan@gmail.com>
2022-06-20 18:19:29 -0300

    changed password now works in deploy

--- client/src/components/ForgotPassword/ForgotPassword.jsx
+++ client/src/components/ForgotPassword/ForgotPassword.jsx
@@ -3,87 +3,92 @@ import { useState } from "react";
 import styles from "./ForgotPassword.module.css";
 
 export default function ForgotPassword() {
+  const [email, setEmail] = useState({
+    email: "",
+  });
+  const [error, setError] = useState({
+    email: "",
+  });
 
-    const [email, setEmail] = useState({
-        email: '',
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  function validateInputs(input) {
+    let error = {
+      email: "",
+    };
+
+    if (input.email === "") {
+      error.email = "Email can't be empty.";
+      console.log("Email vacío");
+    } else if (!emailRegex.test(input.email)) {
+      error.email = "Invalid email format.";
+      console.log("Formato de email inválido");
+    } else {
+      console.log("Email válido");
+      error.email = "";
+    }
+
+    return error;
+  }
+
+  function onChangeHandler(e) {
+    setEmail({
+      ...email,
+      [e.target.name]: e.target.value,
     });
-    const [error, setError] = useState({
-        email: '',
+    let errorResults = validateInputs({
+      ...email,
+      [e.target.name]: e.target.value,
     });
+    setError(errorResults);
+  }
 
-    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  function onSubmitHandler(e) {
+    e.preventDefault();
 
-    function validateInputs(input) {
-
-        let error = {
-            email: '',
-        };
-
-        if(input.email === '') {
-            error.email = "Email can't be empty.";
-            console.log("Email vacío");
-        } else if(!emailRegex.test(input.email)) {
-            error.email = "Invalid email format.";
-            console.log("Formato de email inválido");
-        } else {
-            console.log("Email válido");
-            error.email = "";
-        };
-
-        return error
+    const requirements = {
+      email: email.email,
     };
 
-    function onChangeHandler(e) {
-        setEmail({
-            ...email,
-            [e.target.name]: e.target.value
-        });
-        let errorResults = validateInputs({
-            ...email,
-            [e.target.name]: e.target.value
-        });
-        setError(errorResults);
-    };
+    if (email.email === "") {
+      alert("Please introduce your email first.");
+    }
 
-    function onSubmitHandler(e) {
-        e.preventDefault();
+    if (email.email !== "" && error.email === "") {
+      console.log(`Enviando información: ${email.email}`);
+      axios.post("/api/forgotpassword/", requirements);
+      document.getElementById("submitBtn").className = styles.submitBtnDisabled;
+      document.getElementById("submitBtn").disabled = true;
+      document.getElementById("submitBtn").innerHTML = "Check your email";
+    } else if (error.email !== "") {
+      alert("Email inválido");
+    }
+  }
 
-        const requirements = {
-            email: email.email,
-        };
+  return (
+    <div>
+      <div className={styles.container}>
+        <form className={styles.formContainer}>
+          <label htmlFor="Email">Email</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Introduce your email..."
+            onChange={onChangeHandler}
+            className={styles.emailInput}
+          />
 
-        if(email.email === '') {
-            alert("Please introduce your email first.")
-        };
-
-        if(email.email !== '' && error.email === '') {
-            console.log(`Enviando información: ${email.email}`);
-            axios.post('http://localhost:3001/api/forgotpassword/', requirements);
-            document.getElementById("submitBtn").className = styles.submitBtnDisabled;
-            document.getElementById("submitBtn").disabled = true;
-            document.getElementById("submitBtn").innerHTML = "Check your email";
-        } else if(error.email !== '') {
-            alert("Email inválido");
-        };
-    };
-
-    return(
-        <div>
-            <div className={styles.container}>
-                <form className={styles.formContainer}>
-                    <label htmlFor="Email">Email</label>
-                    <input 
-                        type="text" 
-                        name="email" 
-                        id="email" 
-                        placeholder="Introduce your email..." 
-                        onChange={onChangeHandler}
-                        className={styles.emailInput}
-                        />
-
-                        <button onClick={onSubmitHandler} id="submitBtn" className={styles.submitBtn}>Send restoration email</button>
-                </form>
-            </div>
-        </div>
-    );
-};
+          <button
+            onClick={onSubmitHandler}
+            id="submitBtn"
+            className={styles.submitBtn}
+          >
+            Send restoration email
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
