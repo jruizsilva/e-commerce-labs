065437181f9bae49b7d70a6f89616b1dcd9e8398
jruizsilva <ruizsilvajonathan@gmail.com>
2022-06-25 15:06:30 -0300

    cors added

--- api/src/utils/middlewares/setHeaders.js
+++ api/src/utils/middlewares/setHeaders.js
@@ -1,13 +1,11 @@
 //aca vamos a setear todos nuestros headers
-
 const setHeaders = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept"
-  // );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   res.header("Content-Type", "application/json");
   next();
