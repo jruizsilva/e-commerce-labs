5fecec5a07958a532a8cb3696805e7cb63750894
jruizsilva <ruizsilvajonathan@gmail.com>
2022-06-29 23:23:22 -0300

    fix confirmation page

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,17 +1,14 @@
 import React from "react";
 import Aside from "../Aside/Aside";
 import Products from "../Products/Products";
-import SectionHorizontal from "../SectionHorizontal/SectionHorizontal";
+
 import style from "./Main.module.css";
 
 export default function Main() {
   return (
-    <div className={style.main_container}>
-      <SectionHorizontal />
-      <div className={style.container}>
-        <Aside />
-        <Products />
-      </div>
+    <div className={style.container}>
+      <Aside />
+      <Products />
     </div>
   );
 }
