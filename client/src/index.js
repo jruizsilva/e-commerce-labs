841a50cde699ac2816716d796ce32b10d1cc1815
jruizsilva <ruizsilvajonathan@gmail.com>
2022-06-30 19:21:09 -0300

    index.html modified

--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      {console.log("pull request not working")}
       <App />
     </Provider>
   </BrowserRouter>
